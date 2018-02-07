4d3a71be4c67d53ebb820a44e7de493974fbbc2d
FRC <FRC@IDEA-AM25.boscotech.local>
2018-02-06 16:29:24 -0800

    Added ShuffleBoard stuff to TankDrive

--- src/org/usfirst/frc/team5669/hardware/TankDrive.java
+++ src/org/usfirst/frc/team5669/hardware/TankDrive.java
@@ -1,10 +1,12 @@
 package org.usfirst.frc.team5669.hardware;
 
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TankDrive implements HardwareModule {
 	private SpeedController l1, l2, r1, r2;
-	private double left, right;
+	private double left, right, speed, turn;
 	
 	public TankDrive(SpeedController l1, SpeedController l2, SpeedController r1, SpeedController r2) {
 		this.l1 = l1;
@@ -14,6 +16,8 @@ public class TankDrive implements HardwareModule {
 	}
 	
 	public void set(double speed, double turn) {
+		this.speed = speed;
+		this.turn = turn;
 		left = speed;
 		right = speed;
 		left += turn;
@@ -26,7 +30,14 @@ public class TankDrive implements HardwareModule {
 		l1.set(left);
 		l2.set(left);
 		r1.set(right);
-		r2.set(right);	
+		r2.set(right);
+		SmartDashboard.putNumber("TankDrive.left", left);
+		SmartDashboard.putNumber("TankDrive.right", right);
+		SmartDashboard.putNumber("TankDrive.speed", Math.abs(speed * 100.0));
+		SmartDashboard.putNumber("TankDrive.turn", turn);
+		SmartDashboard.putBoolean("TankDrive.reverse", speed < -0.05);
+		SmartDashboard.putBoolean("TankDrive.forward", speed > 0.03);
+		SmartDashboard.putBoolean("TankDrive.turning", Math.abs(turn) > 0.05);
 	}
 	
 	public void stop() {
